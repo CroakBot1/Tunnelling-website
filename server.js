85c796c88f6f0c800f11b1c15d34ca1782b5754f
CroakBot1 <apploverss1@gmail.com>
2025-12-22 22:43:27 +0800

    Update server.js

--- server.js
+++ server.js
@@ -7,9 +7,7 @@ const app = express();
 const server = http.createServer(app);
 
 const io = new Server(server, {
-  cors: {
-    origin: "*"
-  }
+  cors: { origin: "*" }
 });
 
 app.use(express.static("public"));
@@ -24,14 +22,14 @@ if (fs.existsSync("messages.json")) {
 io.on("connection", (socket) => {
   console.log("User connected");
 
+  // send old messages
   socket.emit("loadMessages", messages);
 
+  // receive new messages
   socket.on("chatMessage", (msg) => {
     console.log("Received:", msg);
-
     messages.push(msg);
     fs.writeFileSync("messages.json", JSON.stringify(messages, null, 2));
-
     io.emit("chatMessage", msg);
   });
 
@@ -41,6 +39,4 @@ io.on("connection", (socket) => {
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
-});
+server.listen(PORT, () => console.log("Server running on port " + PORT));

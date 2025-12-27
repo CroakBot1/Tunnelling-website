5b305ce6b697722d3fa5512151cabc211cdb6486
CroakBot1 <apploverss1@gmail.com>
2025-12-27 23:26:24 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "pwa-cache-v1";
+const CACHE_NAME = "pwa-cache-v3";
 const ASSETS = [
   "/",
   "/index.html",
@@ -8,15 +8,18 @@ const ASSETS = [
 ];
 
 self.addEventListener("install", event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
 });
 
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(
-      response => response || fetch(event.request)
-    )
+    caches.match(event.request).then(res => res || fetch(event.request))
   );
 });

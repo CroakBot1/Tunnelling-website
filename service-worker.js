a61bf0b79710041aa98e9c49be95ef5989667005
CroakBot1 <apploverss1@gmail.com>
2025-12-27 23:19:21 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,9 @@ const CACHE_NAME = "pwa-cache-v1";
 const ASSETS = [
   "/",
   "/index.html",
-  "/manifest.json"
+  "/manifest.json",
+  "/icon-192.png",
+  "/icon-512.png"
 ];
 
 self.addEventListener("install", event => {

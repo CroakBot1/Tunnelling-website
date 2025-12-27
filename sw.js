340890e4c7e756f932c85655fe6680c65cfc2751
CroakBot1 <apploverss1@gmail.com>
2025-12-27 13:50:24 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,25 +2,28 @@ const CACHE_NAME = 'fps-autolock-v1';
 const ASSETS = [
   '/',
   '/index.html',
-  '/manifest.json'
+  '/manifest.json',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
 ];
 
 // INSTALL
 self.addEventListener('install', event => {
+  console.log('SW: Installing...');
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
   self.skipWaiting();
 });
 
-// ACTIVATE (clear old cache)
+// ACTIVATE
 self.addEventListener('activate', event => {
+  console.log('SW: Activating...');
   event.waitUntil(
     caches.keys().then(keys =>
       Promise.all(
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
       )
     )
   );
@@ -30,8 +33,6 @@ self.addEventListener('activate', event => {
 // FETCH
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(cached => {
-      return cached || fetch(event.request);
-    })
+    caches.match(event.request).then(cached => cached || fetch(event.request))
   );
 });
